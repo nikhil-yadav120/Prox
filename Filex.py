6f7d3ca5037fd47ebff5f6f30e4e02f1ab4dfd2c
nikhil-yadav120 <85822159+nikhil-yadav120@users.noreply.github.com>
2021-06-14 18:10:26 +0530

    Update Filex.py

--- Filex.py
+++ Filex.py
@@ -1,5 +1,5 @@
 from instapy import InstaPy
 user = input("Enter username: ")
-pass = input("Enter password: ")
-session = InstaPy(username=user, password=pass)
+pass1 = input("Enter password: ")
+session = InstaPy(username=user, password=pass1)
 session.login()
