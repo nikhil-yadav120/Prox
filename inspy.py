b79fcb84bc0850a2a3bb5d2e7ed340d1e2bf51ef
nikhil-yadav120 <85822159+nikhil-yadav120@users.noreply.github.com>
2021-06-15 13:56:46 +0530

    Create inspy.py

---
+++ inspy.py
@@ -0,0 +1,14 @@
+from instapy import InstaPy
+
+session = InstaPy(username=nikhilyadav3844, password=yadavnikhil**)
+session.login()
+
+session.like_by_tags(["business","adventure","fashion"], amount=10)
+
+session.set_do_follow(True, percentage=25)
+
+session.set_do_comment(True, percentage=50)
+session.set_comments("Nice Post!", "Love It!", "Spectacular Post!")
+
+session.end()
+
